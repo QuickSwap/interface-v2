b2bddabc62f32ca5c5e730b7a33f2f68c552fbd5
totop716 <totop716@gmail.com>
2021-07-28 22:22:13 +0800

    update landing page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -8,8 +8,47 @@ import {
   Button,
   Box,
 } from '@material-ui/core';
+import HeroBkg from 'assets/images/heroBkg.svg';
 
 const useStyles = makeStyles(({ palette, breakpoints }) => ({
+  heroBkg: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%'
+  },
+  heroSection: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: '135px 0 120px',
+    '& h3': {
+      color: 'white',
+      fontSize: 16,
+      textTransform: 'uppercase',
+      fontWeight: 'bold',
+      lineHeight: '18px',
+    },
+    '& h1': {
+      fontSize: 64,
+      fontWeight: 'bold',
+      color: palette.primary.main,
+      lineHeight: '72px',
+      margin: '20px 0',
+    },
+    '& > p': {
+      fontSize: 18,
+      lineHeight: '20px',
+      marginBottom: 50
+    },
+    '& > button': {
+      height: 56,
+      width: 194,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  }
 }));
 
 const LandingPage: React.FC = () => {
@@ -17,6 +56,21 @@ const LandingPage: React.FC = () => {
 
   return (
     <Box>
+      <img src={HeroBkg} alt='Hero Background' className={classes.heroBkg} />
+      <Box className={classes.heroSection}>
+        <Typography component='h3'>
+          Total Value Locked
+        </Typography>
+        <Typography component='h1'>
+          $14,966,289,380
+        </Typography>
+        <Typography>
+          The Top Asset Exchange on the Polygon Network
+        </Typography>
+        <Button color='primary'>
+          <Typography>Connect Wallet</Typography>
+        </Button>
+      </Box>
     </Box>
   );
 };
