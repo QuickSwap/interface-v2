1685a4692be4b67947181d82b3c016a294b8e4c9
totop716 <totop716@gmail.com>
2021-11-22 17:27:51 +0800

    update remove liquidity modal

--- src/components/RemoveLiquidityModal/RemoveLiquidityModal.tsx
+++ src/components/RemoveLiquidityModal/RemoveLiquidityModal.tsx
@@ -4,18 +4,39 @@ import { Contract } from '@ethersproject/contracts';
 import { ArrowLeft, ArrowDown } from 'react-feather';
 import { Box, Typography, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Currency, JSBI, Percent } from '@uniswap/sdk';
+import { Currency, ETHER, JSBI, Percent } from '@uniswap/sdk';
+import ReactGA from 'react-ga';
+import { BigNumber } from '@ethersproject/bignumber';
+import { TransactionResponse } from '@ethersproject/providers';
 import { ROUTER_ADDRESS } from 'constants/index';
-import { CustomModal, ColoredSlider, CurrencyLogo } from 'components';
+import {
+  CustomModal,
+  DoubleCurrencyLogo,
+  ColoredSlider,
+  CurrencyLogo,
+  TransactionConfirmationModal,
+  TransactionErrorContent,
+  ConfirmationModalContent,
+} from 'components';
 import {
   useDerivedBurnInfo,
   useBurnState,
   useBurnActionHandlers,
 } from 'state/burn/hooks';
 import { Field } from 'state/burn/actions';
+import { useUserSlippageTolerance } from 'state/user/hooks';
+import {
+  useTransactionAdder,
+  useTransactionFinalizer,
+} from 'state/transactions/hooks';
 import { useTokenBalance } from 'state/wallet/hooks';
 import { useActiveWeb3React, useIsArgentWallet } from 'hooks';
 import { usePairContract } from 'hooks/useContract';
+import {
+  calculateGasMargin,
+  calculateSlippageAmount,
+  getRouterContract,
+} from 'utils';
 import useDebouncedChangeHandler from 'utils/useDebouncedChangeHandler';
 import useTransactionDeadline from 'hooks/useTransactionDeadline';
 import { useApproveCallback, ApprovalState } from 'hooks/useApproveCallback';
@@ -66,7 +87,14 @@ const RemoveLiquidityModal: React.FC<RemoveLiquidityModalProps> = ({
   onClose,
 }) => {
   const classes = useStyles();
-  const [showConfirm, setShowConfirm] = useState<boolean>(false);
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [txPending, setTxPending] = useState(false);
+  const [attemptingTxn, setAttemptingTxn] = useState(false);
+  const [removeErrorMessage, setRemoveErrorMessage] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+  const [txHash, setTxHash] = useState('');
+  const addTransaction = useTransactionAdder();
+  const finalizedTransaction = useTransactionFinalizer();
   const { chainId, account, library } = useActiveWeb3React();
   const [tokenA, tokenB] = useMemo(
     () => [
@@ -83,6 +111,7 @@ const RemoveLiquidityModal: React.FC<RemoveLiquidityModalProps> = ({
   );
   const deadline = useTransactionDeadline();
   const { onUserInput: _onUserInput } = useBurnActionHandlers();
+  const [allowedSlippage] = useUserSlippageTolerance();
 
   const onUserInput = useCallback(
     (field: Field, typedValue: string) => {
@@ -182,10 +211,15 @@ const RemoveLiquidityModal: React.FC<RemoveLiquidityModalProps> = ({
   const isArgentWallet = useIsArgentWallet();
 
   const onAttemptToApprove = async () => {
-    if (!pairContract || !pair || !library || !deadline)
-      throw new Error('missing dependencies');
+    if (!pairContract || !pair || !library || !deadline) {
+      setErrorMsg('missing dependencies');
+      return;
+    }
     const liquidityAmount = parsedAmounts[Field.LIQUIDITY];
-    if (!liquidityAmount) throw new Error('missing liquidity amount');
+    if (!liquidityAmount) {
+      setErrorMsg('missing liquidity amount');
+      return;
+    }
 
     if (isArgentWallet) {
       return approveCallback();
@@ -249,9 +283,271 @@ const RemoveLiquidityModal: React.FC<RemoveLiquidityModalProps> = ({
       });
   };
 
+  const handleDismissConfirmation = useCallback(() => {
+    setShowConfirm(false);
+    setTxHash('');
+  }, []);
+
+  const onRemove = async () => {
+    if (!chainId || !library || !account || !deadline)
+      throw new Error('missing dependencies');
+    const {
+      [Field.CURRENCY_A]: currencyAmountA,
+      [Field.CURRENCY_B]: currencyAmountB,
+    } = parsedAmounts;
+    if (!currencyAmountA || !currencyAmountB) {
+      throw new Error('missing currency amounts');
+    }
+    const router = getRouterContract(chainId, library, account);
+
+    const amountsMin = {
+      [Field.CURRENCY_A]: calculateSlippageAmount(
+        currencyAmountA,
+        allowedSlippage,
+      )[0],
+      [Field.CURRENCY_B]: calculateSlippageAmount(
+        currencyAmountB,
+        allowedSlippage,
+      )[0],
+    };
+
+    const liquidityAmount = parsedAmounts[Field.LIQUIDITY];
+    if (!liquidityAmount) throw new Error('missing liquidity amount');
+
+    const currencyBIsETH = currency1 === ETHER;
+    const oneCurrencyIsETH = currency0 === ETHER || currencyBIsETH;
+
+    if (!tokenA || !tokenB) throw new Error('could not wrap');
+
+    let methodNames: string[],
+      args: Array<string | string[] | number | boolean>;
+    // we have approval, use normal remove liquidity
+    if (approval === ApprovalState.APPROVED) {
+      // removeLiquidityETH
+      if (oneCurrencyIsETH) {
+        methodNames = [
+          'removeLiquidityETH',
+          'removeLiquidityETHSupportingFeeOnTransferTokens',
+        ];
+        args = [
+          currencyBIsETH ? tokenA.address : tokenB.address,
+          liquidityAmount.raw.toString(),
+          amountsMin[
+            currencyBIsETH ? Field.CURRENCY_A : Field.CURRENCY_B
+          ].toString(),
+          amountsMin[
+            currencyBIsETH ? Field.CURRENCY_B : Field.CURRENCY_A
+          ].toString(),
+          account,
+          deadline.toHexString(),
+        ];
+      }
+      // removeLiquidity
+      else {
+        methodNames = ['removeLiquidity'];
+        args = [
+          tokenA.address,
+          tokenB.address,
+          liquidityAmount.raw.toString(),
+          amountsMin[Field.CURRENCY_A].toString(),
+          amountsMin[Field.CURRENCY_B].toString(),
+          account,
+          deadline.toHexString(),
+        ];
+      }
+    }
+    // we have a signataure, use permit versions of remove liquidity
+    else if (signatureData !== null) {
+      // removeLiquidityETHWithPermit
+      if (oneCurrencyIsETH) {
+        methodNames = [
+          'removeLiquidityETHWithPermit',
+          'removeLiquidityETHWithPermitSupportingFeeOnTransferTokens',
+        ];
+        args = [
+          currencyBIsETH ? tokenA.address : tokenB.address,
+          liquidityAmount.raw.toString(),
+          amountsMin[
+            currencyBIsETH ? Field.CURRENCY_A : Field.CURRENCY_B
+          ].toString(),
+          amountsMin[
+            currencyBIsETH ? Field.CURRENCY_B : Field.CURRENCY_A
+          ].toString(),
+          account,
+          signatureData.deadline,
+          false,
+          signatureData.v,
+          signatureData.r,
+          signatureData.s,
+        ];
+      }
+      // removeLiquidityETHWithPermit
+      else {
+        methodNames = ['removeLiquidityWithPermit'];
+        args = [
+          tokenA.address,
+          tokenB.address,
+          liquidityAmount.raw.toString(),
+          amountsMin[Field.CURRENCY_A].toString(),
+          amountsMin[Field.CURRENCY_B].toString(),
+          account,
+          signatureData.deadline,
+          false,
+          signatureData.v,
+          signatureData.r,
+          signatureData.s,
+        ];
+      }
+    } else {
+      throw new Error(
+        'Attempting to confirm without approval or a signature. Please contact support.',
+      );
+    }
+
+    const safeGasEstimates: (BigNumber | undefined)[] = await Promise.all(
+      methodNames.map((methodName) =>
+        router.estimateGas[methodName](...args)
+          .then(calculateGasMargin)
+          .catch((error) => {
+            console.error(`estimateGas failed`, methodName, args, error);
+            return undefined;
+          }),
+      ),
+    );
+
+    const indexOfSuccessfulEstimation = safeGasEstimates.findIndex(
+      (safeGasEstimate) => BigNumber.isBigNumber(safeGasEstimate),
+    );
+
+    // all estimations failed...
+    if (indexOfSuccessfulEstimation === -1) {
+      console.error('This transaction would fail. Please contact support.');
+    } else {
+      const methodName = methodNames[indexOfSuccessfulEstimation];
+      const safeGasEstimate = safeGasEstimates[indexOfSuccessfulEstimation];
+
+      setAttemptingTxn(true);
+      await router[methodName](...args, {
+        gasLimit: safeGasEstimate,
+      })
+        .then(async (response: TransactionResponse) => {
+          setAttemptingTxn(false);
+          setTxPending(true);
+          const summary =
+            'Remove ' +
+            parsedAmounts[Field.CURRENCY_A]?.toSignificant(3) +
+            ' ' +
+            currency0.symbol +
+            ' and ' +
+            parsedAmounts[Field.CURRENCY_B]?.toSignificant(3) +
+            ' ' +
+            currency1.symbol;
+
+          addTransaction(response, {
+            summary,
+          });
+
+          setTxHash(response.hash);
+
+          try {
+            const receipt = await response.wait();
+            finalizedTransaction(receipt, {
+              summary,
+            });
+            setTxPending(false);
+          } catch (error) {
+            setTxPending(false);
+            setRemoveErrorMessage('There is an error in transaction.');
+          }
+
+          ReactGA.event({
+            category: 'Liquidity',
+            action: 'Remove',
+            label: [currency0.symbol, currency1.symbol].join('/'),
+          });
+        })
+        .catch((error: Error) => {
+          setAttemptingTxn(false);
+          // we only care if the error is something _other_ than the user rejected the tx
+          console.error(error);
+        });
+    }
+  };
+
+  const modalHeader = () => {
+    return (
+      <Box>
+        <Box mt={10} mb={3} display='flex' justifyContent='center'>
+          <DoubleCurrencyLogo
+            currency0={currency0}
+            currency1={currency1}
+            size={48}
+          />
+        </Box>
+        <Box mb={6} color='#c7cad9' textAlign='center'>
+          <Typography variant='h6'>
+            Removing {formattedAmounts[Field.LIQUIDITY]} {currency0.symbol} /{' '}
+            {currency1.symbol} LP Tokens
+            <br />
+            You will receive {parsedAmounts[Field.CURRENCY_A]?.toSignificant(
+              2,
+            )}{' '}
+            {currency0.symbol} and{' '}
+            {parsedAmounts[Field.CURRENCY_B]?.toSignificant(2)}{' '}
+            {currency1.symbol}
+          </Typography>
+        </Box>
+        <Box mb={3} color='#696c80' textAlign='center'>
+          <Typography variant='body2'>
+            {`Output is estimated. If the price changes by more than ${allowedSlippage /
+              100}% your transaction will revert.`}
+          </Typography>
+        </Box>
+        <Box mt={2}>
+          <Button
+            style={{ width: '100%' }}
+            className={classes.removeButton}
+            onClick={onRemove}
+          >
+            Confirm
+          </Button>
+        </Box>
+      </Box>
+    );
+  };
+
   return (
     <CustomModal open={open} onClose={onClose}>
       <Box paddingX={3} paddingY={4}>
+        {showConfirm && (
+          <TransactionConfirmationModal
+            isOpen={showConfirm}
+            onDismiss={handleDismissConfirmation}
+            attemptingTxn={attemptingTxn}
+            txPending={txPending}
+            hash={txHash}
+            content={() =>
+              removeErrorMessage ? (
+                <TransactionErrorContent
+                  onDismiss={handleDismissConfirmation}
+                  message={removeErrorMessage}
+                />
+              ) : (
+                <ConfirmationModalContent
+                  title='Removing Liquidity'
+                  onDismiss={handleDismissConfirmation}
+                  content={modalHeader}
+                />
+              )
+            }
+            pendingText=''
+            modalContent={
+              txPending
+                ? 'Submitted transaction to remove liquidity'
+                : 'Successfully removed liquidity'
+            }
+          />
+        )}
         <Box display='flex' alignItems='center' justifyContent='space-between'>
           <ArrowLeft
             color='#696c80'
@@ -443,6 +739,11 @@ const RemoveLiquidityModal: React.FC<RemoveLiquidityModalProps> = ({
           >
             {error || 'Remove'}
           </Button>
+        </Box>
+        <Box mt={2}>
+          <Typography variant='body1' style={{ color: '#ff5252' }}>
+            {errorMsg}
+          </Typography>
         </Box>
       </Box>
     </CustomModal>
