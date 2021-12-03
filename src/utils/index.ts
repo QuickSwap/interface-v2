5e7f281222bcf35cdf257c9dca6ae12523aeeb79
sameepsi <sameepsi@gmail.com>
2021-12-03 13:45:04 +0530

    Revert "Interim commit"
    This reverts commit 26aa92e8892b41ddd92a17955253d5c7202e372b.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -304,7 +304,7 @@ export const getTopTokens = async (
       current &&
         oneDayData &&
         twoDayData &&
-        current?.data?.tokens?.map(async (token: any) => {
+        current?.data?.tokens.map(async (token: any) => {
           const data = token;
 
           // let liquidityDataThisToken = liquidityData?.[token.id]
