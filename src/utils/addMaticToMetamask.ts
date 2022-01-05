d2f69a0e1a03a8c368ad52d3f9901808e79551d3
totop716 <totop716@gmail.com>
2022-01-04 20:23:04 -0500

    upgrade rpc url when adding polygon to metamask

--- src/utils/addMaticToMetamask.ts
+++ src/utils/addMaticToMetamask.ts
@@ -8,7 +8,7 @@ const addMaticToMetamask: () => void = () => {
           {
             chainId: '0x89',
             chainName: 'Polygon Network',
-            rpcUrls: ['https://rpc-mainnet.maticvigil.com'],
+            rpcUrls: ['https://polygon-rpc.com/'],
             iconUrls: [
               'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0/logo.png',
             ],
