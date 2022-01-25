1b213ed3823476d994e024be2ac54d49a875ca90
sameepsi <sameepsi@gmail.com>
2022-01-25 13:00:36 +0530

    Updated multicall address for mumbai testnet

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@uniswap/sdk';
 import MULTICALL_ABI from './abi.json';
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MUMBAI]: '0x01173890E45aA0f694B68eB89Cb63B295b439FC3', //TODO: CHANGE THIS
+  [ChainId.MUMBAI]: '0xc7efb32470dee601959b15f1f923e017c6a918ca', //TODO: CHANGE THIS
   [ChainId.MATIC]: '0x02817C1e3543c2d908a590F5dB6bc97f933dB4BD',
 };
 
