8bff2fc7397f855ef3d4e2bbae55449c18124f96
totop716 <totop716@gmail.com>
2022-01-28 04:25:14 -0500

    Add beta warning banner.

---
+++ src/components/BetaWarningBanner/BetaWarningBanner.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { AlertTriangle, XCircle } from 'react-feather';
+
+const useStyles = makeStyles(({ palette }) => ({
+  warningBanner: {
+    width: '100%',
+    background: '#e59840',
+    color: palette.background.default,
+    position: 'relative',
+    zIndex: 2,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '8px 32px 8px 8px',
+    '& > span': {
+      marginLeft: 4,
+      fontWeight: 'bold',
+      textTransform: 'uppercase',
+      maxWidth: 'calc(100% - 24px)',
+    },
+  },
+  closeBanner: {
+    position: 'absolute',
+    right: 12,
+    top: '50%',
+    transform: 'translateY(-50%)',
+    display: 'flex',
+  },
+}));
+
+const BetaWarningBanner: React.FC = () => {
+  const classes = useStyles();
+  const [showBanner, setShowBanner] = useState(true);
+  return (
+    <>
+      {showBanner && (
+        <Box className={classes.warningBanner}>
+          <AlertTriangle size={20} />
+          <Typography variant='caption'>
+            This site is in beta. PLEASE Use it at your own risk.
+          </Typography>
+          <Box
+            onClick={() => setShowBanner(false)}
+            className={classes.closeBanner}
+          >
+            <XCircle size={20} />
+          </Box>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default BetaWarningBanner;
