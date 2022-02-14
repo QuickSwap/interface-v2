a66a1e9cd174ba81e32bf789cec06336b7086810
totop716 <totop716@gmail.com>
2022-02-13 22:08:38 -0500

    remove unnecessary properties from syrup and add reusable function to calculate dQUICK APY.

--- src/pages/LandingPage/TradingInfo.tsx
+++ src/pages/LandingPage/TradingInfo.tsx
@@ -1,10 +1,10 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { Typography, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Skeleton } from '@material-ui/lab';
 import { StakeQuickModal } from 'components';
 import { useLairInfo, useTotalRewardsDistributed } from 'state/stake/hooks';
-import { getDaysCurrentYear, formatCompact } from 'utils';
+import { formatCompact, useLairDQUICKAPY } from 'utils';
 
 const useStyles = makeStyles(({ palette, breakpoints }) => ({
   tradingSection: {
