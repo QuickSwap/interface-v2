90f8590362fdc4da22d7bd9d3cdd6eceb678ee41
totop716 <totop716@gmail.com>
2022-04-10 12:48:04 -0400

    update quick conversion UI

--- src/pages/ConvertQUICKPage/ConvertQUICKPage.tsx
+++ src/pages/ConvertQUICKPage/ConvertQUICKPage.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import cx from 'classnames';
 import { Box, Typography, Button } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import QUICKIcon from 'assets/images/quickIcon.svg';
 import { ReactComponent as QUICKV2Icon } from 'assets/images/QUICKV2.svg';
 import { ArrowForward, ArrowDownward } from '@material-ui/icons';
 import { NumericalInput } from 'components';
+import { returnTokenFromKey } from 'utils';
+import { useTokenBalance } from 'state/wallet/hooks';
+import { useActiveWeb3React } from 'hooks';
 
 const useStyles = makeStyles(({ palette, breakpoints }) => ({
   wrapper: {
@@ -46,6 +50,7 @@ const useStyles = makeStyles(({ palette, breakpoints }) => ({
     alignItems: 'center',
     height: 63,
     padding: '0 16px',
+    border: '1px solid transparent',
     '& input': {
       flex: 1,
     },
@@ -64,24 +69,48 @@ const useStyles = makeStyles(({ palette, breakpoints }) => ({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    cursor: 'pointer',
   },
   convertButton: {
     maxWidth: 224,
     height: 40,
     width: '100%',
     borderRadius: 20,
+    '&.Mui-disabled': {
+      background: palette.grey.A400,
+    },
+  },
+  errorInput: {
+    borderColor: palette.error.main,
+  },
+  errorText: {
+    color: palette.error.main,
   },
 }));
 
 const ConvertQUICKPage: React.FC = () => {
   const classes = useStyles();
   const { t } = useTranslation();
+  const { account } = useActiveWeb3React();
   const [quickAmount, setQUICKAmount] = useState('');
   const [quickV2Amount, setQUICKV2Amount] = useState('');
+  const quickToken = returnTokenFromKey('QUICK');
+  const quickBalance = useTokenBalance(account ?? undefined, quickToken);
+  const isInsufficientQUICK =
+    Number(quickAmount) > Number(quickBalance?.toExact() ?? 0);
+  const buttonText = useMemo(() => {
+    if (isInsufficientQUICK) {
+      return t('insufficientBalance');
+    } else if (quickAmount === '') {
+      return t('enterAmount');
+    } else {
+      return t('convert');
+    }
+  }, [isInsufficientQUICK, quickAmount, t]);
 
   return (
     <Box width='100%' maxWidth={488} id='convertQUICKPage'>
-      <Typography variant='h4'>Convert QUICK</Typography>
+      <Typography variant='h4'>{t('convert')} QUICK</Typography>
       <Box className={classes.wrapper}>
         <Box display='flex' alignItems='center' mb={3}>
           <Box className={classes.iconWrapper}>
@@ -97,39 +126,60 @@ const ConvertQUICKPage: React.FC = () => {
           <Typography variant='h6'>QUICK-v2</Typography>
         </Box>
         <Typography variant='body2' color='textSecondary'>
-          Convert your QUICK to QUICK-v2. Lorem ipsum dolor sit amet
-          constracteur amit seiu. You have until 00 JUNE 2024 to convert.
+          {t('convertQUICK')}
         </Typography>
         <Box className={classes.conversionRate}>
           <Typography variant='caption'>
-            Conversion Rate: 1 QUICK = 100 QUICK-v2
+            {t('conversionRate')}: 1 QUICK = 100 QUICK-v2
           </Typography>
         </Box>
         <Box mt={4} mb={2}>
           <Typography variant='body2' color='textSecondary'>
-            Your balance: 10
+            {t('yourbalance')}: {quickBalance?.toSignificant(2)}
           </Typography>
-          <Box className={classes.currencyInput}>
+          <Box
+            className={cx(
+              classes.currencyInput,
+              isInsufficientQUICK && classes.errorInput,
+            )}
+          >
             <NumericalInput
               placeholder='0.00'
               value={quickAmount}
               fontSize={18}
               onUserInput={(value) => {
                 setQUICKAmount(value);
+                setQUICKV2Amount((Number(value) * 100).toString());
               }}
             />
-            <Box mr={1} className={classes.maxButton}>
+            <Box
+              mr={1}
+              className={classes.maxButton}
+              onClick={() => {
+                if (quickBalance) {
+                  setQUICKAmount(quickBalance.toExact());
+                  setQUICKV2Amount(
+                    (Number(quickBalance.toExact()) * 100).toString(),
+                  );
+                }
+              }}
+            >
               {t('max')}
             </Box>
             <Typography variant='h6'>QUICK</Typography>
           </Box>
+          {isInsufficientQUICK && (
+            <Typography variant='body2' className={classes.errorText}>
+              {t('insufficientBalance', { symbol: 'QUICK' })}
+            </Typography>
+          )}
         </Box>
         <Box ml={2} className={classes.convertArrow}>
           <ArrowDownward />
         </Box>
         <Box mt={2} mb={4}>
           <Typography variant='body2' color='textSecondary'>
-            You will receive:
+            {t('youwillreceive')}:
           </Typography>
           <Box className={classes.currencyInput}>
             <NumericalInput
@@ -138,13 +188,19 @@ const ConvertQUICKPage: React.FC = () => {
               fontSize={18}
               onUserInput={(value) => {
                 setQUICKV2Amount(value);
+                setQUICKAmount((Number(value) / 100).toString());
               }}
             />
             <Typography variant='h6'>QUICK-v2</Typography>
           </Box>
         </Box>
         <Box display='flex' justifyContent='center'>
-          <Button className={classes.convertButton}>Convert</Button>
+          <Button
+            disabled={isInsufficientQUICK || quickAmount === ''}
+            className={classes.convertButton}
+          >
+            {buttonText}
+          </Button>
         </Box>
       </Box>
     </Box>
