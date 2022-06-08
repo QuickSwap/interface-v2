421441bbbbfa1fb76ee4a59352f9004952f5a947
totop716 <totop716@gmail.com>
2022-06-08 15:07:33 -0400

    add new tokens, syrups and farms

--- src/constants/lists.ts
+++
@@ -1,5 +0,0 @@
-// the Quickswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL =
-  'https://unpkg.com/quickswap-default-token-list@1.2.34/build/quickswap-default.tokenlist.json';
-
-export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL];
