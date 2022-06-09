a2527139bc0dc3adb45f92736f6d9820fac8e72b
RitzyDevBox <RitzyDevBox@gmail.com>
2022-06-09 01:56:03 -0400

    updates to prdt styles

--- src/pages/PrdtPage/PrdtPage.tsx
+++ src/pages/PrdtPage/PrdtPage.tsx
@@ -8,21 +8,21 @@ const PrdtPage: React.FC = () => {
   const { t } = useTranslation();
   return (
     <Box width='100%' id='PrdtPage'>
-      <div className='externalDappWarningBanner'>
-        <AlertTriangle size={20} />
-        <span className='text-bold'> {t('externalDappWarning')}</span>
-      </div>
       <div style={{ height: '100%', width: '100%', overflowY: 'auto' }}>
         <iframe
           title='Predictions Market'
           allow='payment'
-          frameBorder='1px solid red'
+          frameBorder='0'
           height='900px'
-          src='https://cloudflare-ipfs.com/ipfs/Qma9BHGWNxFYWrQMU2oFme8GFqEo15ehTpE6Qpzy7Zh1cq/'
+          src={process.env.REACT_APP_PRDT_URL}
           width='100%'
         >
           <p>Your browser does not support iframes</p>
         </iframe>
+      </div>
+      <div className='externalDappWarningBanner'>
+        <AlertTriangle size={20} />
+        <span className='text-bold'> {t('prdtWarning')}</span>
       </div>
     </Box>
   );
