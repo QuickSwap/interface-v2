3d8ac4f308330c76d63e52f4cdde1284f9347424
RitzyDevBox <RitzyDevBox@gmail.com>
2022-06-21 16:15:21 -0400

    applying format to all files

--- cypress/integration/landing.test.ts
+++ cypress/integration/landing.test.ts
@@ -1,39 +1,38 @@
-import { TEST_ADDRESS_NEVER_USE_SHORTENED } from '../support/commands'
+import { TEST_ADDRESS_NEVER_USE_SHORTENED } from '../support/commands';
 
 describe('Landing Page', () => {
-  beforeEach(() => cy.visit('/'))
+  beforeEach(() => cy.visit('/'));
   it('loads landing page', () => {
-    cy.get('#landing-page')
-  })
-
+    cy.get('#landing-page');
+  });
 
   it('allows navigation to pool', () => {
-    cy.get('#pools-page-link').click()
-    cy.url().should('include', '/pools')
-  })
+    cy.get('#pools-page-link').click();
+    cy.url().should('include', '/pools');
+  });
 
   it('allows navigation to swap', () => {
-    cy.get('#swap-page-link').click()
-    cy.url().should('include', '/swap')
-  })
+    cy.get('#swap-page-link').click();
+    cy.url().should('include', '/swap');
+  });
 
   it('allows navigation to farm', () => {
-    cy.get('#farm-page-link').click()
-    cy.url().should('include', '/farm')
-  })
+    cy.get('#farm-page-link').click();
+    cy.url().should('include', '/farm');
+  });
 
   it('allows navigation to dragons lair', () => {
-    cy.get('#dragons-page-link').click()
-    cy.url().should('include', '/dragons')
-  })
+    cy.get('#dragons-page-link').click();
+    cy.url().should('include', '/dragons');
+  });
 
   it('allows navigation to analytics', () => {
-    cy.get('#analytics-page-link').click()
-    cy.url().should('include', '/analytics')
-  })
+    cy.get('#analytics-page-link').click();
+    cy.url().should('include', '/analytics');
+  });
 
   /**it('is connected', () => {
     cy.get('#web3-status-connected').click()
     cy.get('#web3-account-identifier-row').contains(TEST_ADDRESS_NEVER_USE_SHORTENED)
   })*/
-})
+});
