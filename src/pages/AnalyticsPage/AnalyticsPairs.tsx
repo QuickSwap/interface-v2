80a36c9e1c777b16d8a6aee3510c385b90f23c4b
lilchizh <xlilchizhx@gmail.com>
2022-07-31 22:02:37 +0300

    (pairs-page) Adjust fetch function

--- src/pages/AnalyticsPage/AnalyticsPairs.tsx
+++ src/pages/AnalyticsPage/AnalyticsPairs.tsx
@@ -6,30 +6,36 @@ import { Skeleton } from '@material-ui/lab';
 import { useTranslation } from 'react-i18next';
 import { GlobalConst } from 'constants/index';
 import { useEthPrice } from 'state/application/hooks';
+import { useIsV3 } from 'state/analytics/hooks';
+import { getTopPairsV3 } from 'utils/v3-graph';
 
 const AnalyticsPairs: React.FC = () => {
   const { t } = useTranslation();
   const [topPairs, updateTopPairs] = useState<any[] | null>(null);
   const { ethPrice } = useEthPrice();
 
+  const isV3 = useIsV3();
+
   useEffect(() => {
     const fetchTopPairs = async () => {
       updateTopPairs(null);
-      const pairs = await getTopPairs(GlobalConst.utils.ANALYTICS_PAIRS_COUNT);
-      const formattedPairs = pairs
-        ? pairs.map((pair: any) => {
-            return pair.id;
-          })
-        : [];
-      const pairData = await getBulkPairData(formattedPairs, ethPrice.price);
-      if (pairData) {
-        updateTopPairs(pairData);
-      }
+      const topPairsFn = isV3
+        ? getTopPairsV3(GlobalConst.utils.ANALYTICS_PAIRS_COUNT)
+        : getBulkPairData(
+            GlobalConst.utils.ANALYTICS_PAIRS_COUNT,
+            ethPrice.price,
+          );
+
+      topPairsFn.then((data) => {
+        if (data) {
+          updateTopPairs(data);
+        }
+      });
     };
     if (ethPrice.price) {
       fetchTopPairs();
     }
-  }, [updateTopPairs, ethPrice.price]);
+  }, [updateTopPairs, ethPrice.price, isV3]);
 
   return (
     <Box width='100%' mb={3}>
