8f2770d9b4ffc8906a20a7e8dd9f5c2cf7707ca2
totop716 <totop716@gmail.com>
2022-08-07 22:03:54 -0400

    remove unused hook and fetch extra pool data in real time

--- src/hooks/marketxyz/usePoolData.ts
+++ src/hooks/marketxyz/usePoolData.ts
@@ -73,35 +73,3 @@ export const usePoolData = (
 
   return data;
 };
-
-export const usePoolDataComptroller = (
-  comptroller: Comptroller | string,
-  directory: PoolDirectoryV1 | string,
-) => {
-  const { account } = useActiveWeb3React();
-  const { sdk } = useMarket();
-  const [data, setData] = useState<PoolData>();
-
-  useEffect(() => {
-    if (!sdk) {
-      return;
-    }
-    const _directory =
-      typeof directory === 'string'
-        ? new PoolDirectoryV1(sdk, directory)
-        : directory;
-
-    (async () => {
-      const poolId = await getPoolIdFromComptroller(comptroller, _directory);
-      const data = await fetchPoolData(
-        poolId,
-        account ?? undefined,
-        _directory,
-      );
-
-      return data;
-    })();
-  }, [sdk, account, comptroller, directory]);
-
-  return data;
-};
