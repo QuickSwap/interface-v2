f6a38162d990dad5a3f97129d488df260a21d9d7
RitzyDevBox <RitzyDevBox@gmail.com>
2022-08-22 22:43:43 -0400

    adding minimal tick range to farming contracts

--- src/v3lib/utils/v3constants.ts
+++ src/v3lib/utils/v3constants.ts
@@ -1,13 +1,7 @@
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
 
 export const POOL_INIT_CODE_HASH =
-  '0xe1970ade7abdb2ad0709066b5086cc324cd391f945aab88b824d6a42e7646c51';
-export const POOL_INIT_CODE_HASH_OPTIMISM =
-  '0x0c231002d0970d2126e7e00ce88c3b0e5ec8e48dac71478d56245c34ea2f9447';
-
-// historical artifact due to small compiler mismatch
-export const POOL_INIT_CODE_HASH_OPTIMISM_KOVAN =
-  '0x75540cffbf6fa0f7384d7cd847b36e21a896b818798ea322666054ba953ae16a';
+  '0x6ec6c9c8091d160c0aa74b2b14ba9c1717e95093bd3ac085cee99a49aab294a4';
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
