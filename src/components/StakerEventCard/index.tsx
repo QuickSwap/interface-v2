1e162bc77060b34a69aa607a2c7156976d61542d
RitzyDevBox <RitzyDevBox@gmail.com>
2022-08-24 22:40:24 -0400

    Applying Farm styles

--- src/components/StakerEventCard/index.tsx
+++ src/components/StakerEventCard/index.tsx
@@ -15,6 +15,15 @@ import './index.scss';
 import { Link } from 'react-router-dom';
 import { ChainId } from '@uniswap/sdk';
 import EternalFarm from './EternalFarmCard';
+import { DoubleCurrencyLogo, Logo } from 'components';
+import {
+  StyledButton,
+  StyledDarkBox,
+  StyledFilledBox,
+  StyledLabel,
+} from 'components/v3/Common/styledElements';
+import { ReactComponent as AddIcon } from 'assets/images/addIcon.svg';
+import { Box } from '@material-ui/core';
 
 interface FarmingEventCardProps {
   active?: boolean;
@@ -99,248 +108,114 @@ export function FarmingEventCard({
   }, [reward, bonusReward, rewardToken, bonusRewardToken]);
 
   return (
-    <div
-      className={'farming-event-card p-1 br-12'}
-      data-refreshing={refreshing}
-    >
+    <>
       {refreshing && (
         <LoadingShim>
           <Loader size={'18px'} stroke={'white'} style={{ margin: 'auto' }} />
         </LoadingShim>
       )}
-      {!refreshing && <EternalFarm />}
-      {locked && (
-        <LoadingShim>
-          <div
-            style={{
-              padding: '1rem',
-              background: '#fff',
-              borderRadius: '8px',
-              color: 'black',
-            }}
+      {!refreshing && (
+        <Box ml={1.5} mr={1.5}>
+          <StyledFilledBox
+            padding={1.5}
+            width={336}
+            // height={218}
+            className='flex flex-col'
           >
-            <div className='mb-1 f f-ac f-jc'>
-              <span className='mr-05'>
-                <Lock size={'16px'} stroke={'black'} />
-              </span>
-              <span>This farm is filled</span>
-            </div>
-            <div>
-              <Link
-                to={'/farming/infinite-farms'}
-                className={'farming-event-card__infinite-farming-available'}
-                style={{
-                  padding: '6px 10px',
-                  borderRadius: '8px',
-                  color: 'var(--white)',
-                  background: 'var(--primary)',
-                }}
-              >
-                Infinite WETH farm is available →
-              </Link>
-            </div>
-          </div>
-        </LoadingShim>
-      )}
-      <div className={'f mb-1'}>
-        <div className={'f mr-1'}>
-          <CurrencyLogo
-            currency={
-              new Token(
-                ChainId.MATIC,
-                pool.token0.id,
-                18,
-                pool.token0.symbol,
-              ) as WrappedCurrency
-            }
-            size={'30px'}
-          />
-          <CurrencyLogo
-            currency={
-              new Token(
-                ChainId.MATIC,
-                pool.token1.id,
-                18,
-                pool.token1.symbol,
-              ) as WrappedCurrency
-            }
-            size={'30px'}
-          />
-        </div>
-        <div>
-          <h3 className={'fs-075 b'}>POOL</h3>
-          <div
-            style={{ marginTop: '2px' }}
-          >{`${pool.token0.symbol}/${pool.token1.symbol}`}</div>
-        </div>
-        {apr && apr > 0 ? (
-          <div
-            className={'farming-event-card__reward-apr p-05 br-8 ml-a fs-085'}
-          >
-            <span>{Math.round(apr)}%</span>
-            <span style={{ marginLeft: '5px' }}>APR</span>
-          </div>
-        ) : null}
-      </div>
-      <div className={'farming-event-card__reward-wrapper mb-05 f c br-8'}>
-        <div className='farming-event-card__reward-wrapper-header fs-075 b'>
-          REWARDS
-        </div>
-        <ul className='farming-event-card__reward-list'>
-          {rewardList?.map((reward: any, i) => (
-            <li key={i} className='farming-event-card__reward-list-item f'>
-              <CurrencyLogo
-                currency={
-                  new Token(
-                    ChainId.MATIC,
-                    reward.token.id,
-                    18,
-                    reward.token.symbol,
-                  ) as WrappedCurrency
-                }
-                size={'30px'}
-              />
-              <span className='farming-event-card__reward-list-item__symbol ml-05'>
-                {reward.token.symbol}
-              </span>
-              <div
-                className={'m-a mr-0 fs-085'}
-                title={reward.amount.toString()}
-              >
-                {eternal ? (
-                  <span />
-                ) : (
-                  <span>{formatAmountTokens(reward.amount, false)}</span>
+            <Box mb={1.5} className='flex justify-starts items-center'>
+              <Box className='flex items-center'>
+                <DoubleCurrencyLogo
+                  currency0={
+                    new Token(
+                      ChainId.MATIC,
+                      pool.token0.id,
+                      18,
+                      pool.token0.symbol,
+                    ) as WrappedCurrency
+                  }
+                  currency1={
+                    new Token(
+                      ChainId.MATIC,
+                      pool.token1.id,
+                      18,
+                      pool.token1.symbol,
+                    ) as WrappedCurrency
+                  }
+                  size={30}
+                />
+
+                <StyledLabel className='ml-1' fontSize='14px' color='##ebecf2'>
+                  {`${pool.token0.symbol}/${pool.token1.symbol}`}
+                </StyledLabel>
+              </Box>
+            </Box>
+
+            {rewardList?.map((reward: any, i) => (
+              <>
+                <StyledDarkBox
+                  padding={1.5}
+                  className='flex items-center '
+                  height={56}
+                >
+                  <Box className='flex'>
+                    <CurrencyLogo
+                      currency={
+                        new Token(
+                          ChainId.MATIC,
+                          reward.token.id,
+                          18,
+                          reward.token.symbol,
+                        ) as WrappedCurrency
+                      }
+                      size={'30px'}
+                    />
+
+                    <Box className='flex-col' ml={1.5}>
+                      <StyledLabel color='#696c80' fontSize='12px'>
+                        {reward.token.symbol}
+                      </StyledLabel>
+                      <StyledLabel color='#ebecf2' fontSize='14px'>
+                        {formatAmountTokens(reward.amount, false)}
+                      </StyledLabel>
+                    </Box>
+                  </Box>
+                </StyledDarkBox>
+                {i + 1 == rewardList.length && (
+                  <Box
+                    className='flex justify-center'
+                    mt={-1.5}
+                    mb={-1.5}
+                    zIndex={1}
+                  >
+                    <AddIcon />
+                  </Box>
                 )}
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-      {!eternal && (
-        <div className='w-100 farming-event-card__timeline'>
-          <div className='w-100 f farming-event-card__timeline-dates'>
-            <div className='w-100 b fs-075 ta-l'>Enter</div>
-            <div className='w-100 b fs-075 ta-c'>Start</div>
-            <div className='w-100 b fs-075 ta-r'>End</div>
-          </div>
-          <div className='w-100 f mt-05'>
-            <div className='f f-ac f-jc farming-event-card__timeline-circle'>
-              <div
-                className={`farming-event-card__timeline-circle__inner ${
-                  !active ? 'active' : ''
-                }`}
-              ></div>
-            </div>
-            <div className='farming-event-card__timeline-line'>
-              <div
-                className='farming-event-card__timeline-line__inner'
-                style={{
-                  width: active
-                    ? '100%'
-                    : new Date(enterStartTime! * 1000).getTime() >= Date.now()
-                    ? '0%'
-                    : `${getProgress(
-                        Number(createdAtTimestamp),
-                        startTime,
-                        now,
-                      )}%`,
-                }}
-              ></div>
-            </div>
-            <div className='f f-ac f-jc farming-event-card__timeline-circle'>
-              {active && (
-                <div className='farming-event-card__timeline-circle__inner active' />
-              )}
-            </div>
-            <div className='farming-event-card__timeline-line'>
-              {active && (
-                <div
-                  className='w-100 farming-event-card__timeline-line__inner'
-                  style={{ width: `${getProgress(startTime, endTime, now)}%` }}
-                ></div>
-              )}
-            </div>
-            <div className='farming-event-card__timeline-circle'></div>
-          </div>
-          <div className='w-100 f fs-085' style={{ marginTop: '10px' }}>
-            <div className='w-100 f f-ac'>
-              <div className='farming-event-card__timeline-calendar first ta-c'>
-                <div className='farming-event-card__timeline-calendar-day'>
-                  {_enterTime[0]}
-                </div>
-                <div className='farming-event-card__timeline-calendar-hour'>
-                  {_enterTime[1]}
-                </div>
-              </div>
-            </div>
-            <div className='w-100 f f-ac f-jc'>
-              <div className='farming-event-card__timeline-calendar second ta-c'>
-                <div className='farming-event-card__timeline-calendar-day'>
-                  {_startTime[0]}
-                </div>
-                <div className='farming-event-card__timeline-calendar-hour'>
-                  {_startTime[1]}
-                </div>
-              </div>
-            </div>
-            <div
-              className='w-100 f f-jc'
-              style={{ justifyContent: 'flex-end' }}
+              </>
+            ))}
+
+            <StyledDarkBox
+              className='flex justify-between items-center mt-1'
+              height={56}
+              padding={1.5}
             >
-              <div className='farming-event-card__timeline-calendar third ta-c'>
-                <div className='farming-event-card__timeline-calendar-day'>
-                  {_endTime[0]}
-                </div>
-                <div className='farming-event-card__timeline-calendar-hour'>
-                  {_endTime[1]}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+              <StyledLabel color='#696c80' fontSize='12px'>
+                Overall APR:
+              </StyledLabel>
+              <StyledLabel color='#0fc679' fontSize='14px'>
+                {apr && apr > 0 && apr !== NaN ? Math.round(apr) : 'Unknown'}%
+              </StyledLabel>
+            </StyledDarkBox>
+
+            <Box marginTop={2}>
+              <StyledButton height='40px' onClick={farmHandler}>
+                <StyledLabel color='#ebecf2' fontSize='14px'>
+                  Farm
+                </StyledLabel>
+              </StyledButton>
+            </Box>
+          </StyledFilledBox>
+        </Box>
       )}
-      {account && !active ? (
-        <button
-          style={{
-            marginTop: '9px',
-            border: 'none',
-            lineHeight: '19px',
-            height: '36px',
-          }}
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          //@ts-ignore
-          disabled={
-            locked ||
-            new Date((enterStartTime ?? 0) * 1000).getTime() >= Date.now()
-          }
-          className={`btn primary w-100 b br-8 fs-085 pv-05 ${
-            !eternal ? 'mt-05' : ''
-          }`}
-          onClick={farmHandler}
-        >
-          <span>{locked ? `Filled` : `Farm`}</span>
-        </button>
-      ) : active ? (
-        <div
-          className={'mt-1 fs-085 p-05 br-8 ta-c mt-1 bg-pw'}
-          style={{
-            marginTop: '9px',
-            border: 'none',
-            lineHeight: '19px',
-            height: '36px',
-          }}
-        >
-          Started!
-        </div>
-      ) : (
-        <button
-          className={`btn primary w-100 b pv-05 ${!eternal ? 'mt-05' : ''}`}
-          onClick={toggleWalletModal}
-        >
-          Connect Wallet
-        </button>
-      )}
-    </div>
+    </>
   );
 }
