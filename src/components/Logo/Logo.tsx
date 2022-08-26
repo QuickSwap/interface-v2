239f99fbc1f3e126319daa9b583bc1289be2ee93
RitzyDevBox <RitzyDevBox@gmail.com>
2022-08-25 21:50:15 -0400

    more fixes and debugging issues

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -7,14 +7,22 @@ const BAD_SRCS: { [tokenAddress: string]: true } = {};
 export interface LogoProps {
   srcs: string[];
   alt?: string;
+  symbol?: string;
   size?: string;
 }
 
-const Logo: React.FC<LogoProps> = ({ srcs, alt, size = '24px' }) => {
+const Logo: React.FC<LogoProps> = ({
+  srcs,
+  alt,
+  symbol = '??',
+  size = '24px',
+}) => {
   const [, refresh] = useState<number>(0);
-
+  const fontSize = parseInt(size, 10) >= 24 ? '12px' : '8px';
   const src: string | undefined = srcs.find((src) => !BAD_SRCS[src]);
 
+  symbol = symbol.trim();
+  const displaySymbol = symbol.slice(0, 1) + symbol.slice(-1);
   if (src) {
     return (
       <img
@@ -35,11 +43,10 @@ const Logo: React.FC<LogoProps> = ({ srcs, alt, size = '24px' }) => {
         background: stringToColour(alt ?? '').background,
         color: stringToColour(alt ?? '').text,
         border: stringToColour(alt ?? '').border,
-
-        fontSize: size === '18px' ? '8px' : size === '24px' ? '12px' : '10px',
+        fontSize: fontSize,
       }}
     >
-      {alt?.slice(0, 3).trimEnd()}
+      {displaySymbol}
     </StyledLogo>
   );
 };
