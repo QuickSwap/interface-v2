d91c56d14e902544f0f3cb7301271b29b3ccbba3
RitzyDevBox <RitzyDevBox@gmail.com>
2022-09-10 19:53:13 -0400

    hiding apy for now

--- src/pages/DragonPage/DragonsLair.tsx
+++ src/pages/DragonPage/DragonsLair.tsx
@@ -69,10 +69,12 @@ const DragonsLair: React.FC<{ isNew: boolean }> = ({ isNew }) => {
           ).toLocaleString()}
         </small>
       </Box>
+
       <Box className='dragonLairRow'>
         <small>{t('apy')}</small>
-        <small className='text-success'>{APY}%</small>
+        <small className='text-success'>{isNew ? 'TBD' : `${APY}%`}</small>
       </Box>
+
       <Box className='dragonLairRow'>
         <small>{t('yourdeposits')}</small>
         <small>{formatTokenAmount(lairInfoToUse.QUICKBalance)}</small>
