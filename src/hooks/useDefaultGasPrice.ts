8cb95f767c6226bfc654d4e07e2032d358260443
Ritzy Dev <RitzyDevBox@gmail.com>
2022-10-02 18:14:40 -0700

    Adding work around to have better default gas price for Dogechain

--- src/hooks/useDefaultGasPrice.ts
+++ src/hooks/useDefaultGasPrice.ts
@@ -12,15 +12,22 @@ export function useDefaultGasPrice(chainId: ChainId | undefined): number {
   const defaultUnknownChainGasPrice = 75;
   const chainInfo = chainId ? CHAIN_INFO[chainId] : undefined;
 
-  if (!chainInfo) {
-    console.log(
-      `No chain info for chainId: ${chainId} overriding default gas price to  ${defaultUnknownChainGasPrice}`,
-    );
-  }
-
-  const defaultGasPrice =
-    chainInfo?.defaultGasPrice ?? defaultUnknownChainGasPrice;
   const gasPrice = useAppSelector((state) => {
+    if (!chainInfo) {
+      console.log(
+        `No chain info for chainId: ${chainId} overriding default gas price to  ${defaultUnknownChainGasPrice}`,
+      );
+    }
+
+    const defaultGasPrice =
+      chainInfo?.defaultGasPrice ?? defaultUnknownChainGasPrice;
+
+    // TODO: This is a work around since our gas update currently only works with matic
+    // Remove this once, we have a gas update for dogechain
+    if (chainId != ChainId.MATIC) {
+      return defaultGasPrice;
+    }
+
     if (!state.application.gasPrice.fetched) {
       return defaultGasPrice;
     }
