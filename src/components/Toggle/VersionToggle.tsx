0269cc87ad7472310f3c0e9964b8c19110df4019
webdev403 <web.dev8231@gmail.com>
2022-12-08 11:37:14 +0800

    feat: redirect with links when switching tokens in pools page

--- src/components/Toggle/VersionToggle.tsx
+++ src/components/Toggle/VersionToggle.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { useHistory, useParams } from 'react-router-dom';
 import './index.scss';
 import { useIsV2 } from 'state/application/hooks';
+import { GlobalConst } from 'constants/index';
 
 const VersionToggle: React.FC = () => {
   const { t } = useTranslation();
@@ -17,30 +18,39 @@ const VersionToggle: React.FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [version]);
 
+  const redirectWithVersion = (version: string) => {
+    const newQuickAddress = GlobalConst.addresses.NEW_QUICK_ADDRESS;
+    const versionParam = params ? params.version : undefined;
+    const currencyIdAParam = params ? params.currencyIdA : undefined;
+    const currencyIdBParam = params ? params.currencyIdB : undefined;
+    const redirectPathName = versionParam
+      ? history.location.pathname.replace('/' + versionParam, `/${version}`)
+      : history.location.pathname +
+        (history.location.pathname.includes('/add')
+          ? (currencyIdAParam ? '' : `/ETH`) +
+            (currencyIdBParam ? '' : `/${newQuickAddress}`)
+          : '') +
+        `/${version}`;
+    history.push(
+      redirectPathName +
+        (history.location.pathname.includes('/pools')
+          ? history.location.search
+          : ''),
+    );
+  };
+
   return (
     <Box className='version-toggle-container'>
       <Box
         className={isV2 ? 'version-toggle-active' : ''}
-        onClick={() => {
-          history.push(
-            params && params.version
-              ? history.location.pathname.replace('/' + params.version, '/v2')
-              : history.location.pathname + '/v2',
-          );
-        }}
+        onClick={() => redirectWithVersion('v2')}
       >
         <small>{t('V2')}</small>
       </Box>
 
       <Box
         className={!isV2 ? 'version-toggle-active' : ''}
-        onClick={() => {
-          history.push(
-            params && params.version
-              ? history.location.pathname.replace('/' + params.version, '/v3')
-              : history.location.pathname + '/v3',
-          );
-        }}
+        onClick={() => redirectWithVersion('v3')}
       >
         <small>{t('V3')}</small>
       </Box>
