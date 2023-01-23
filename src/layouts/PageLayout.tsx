230b97d631aab5f66098de770c966af726450f8d
totop716 <totop716@gmail.com>
2023-01-22 23:11:41 -0500

    update dev password

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -54,7 +54,7 @@ const PageLayout: React.FC<PageLayoutProps> = ({ children, name }) => {
   const PasswordModal = () => {
     const [devPass, setDevPass] = useState('');
     const confirmPassword = () => {
-      if (devPass === 'devPass') {
+      if (devPass === 'gammaPass') {
         setOpenPassModal(false);
       }
     };
