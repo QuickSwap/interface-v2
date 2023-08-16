c8081fdd6fd99a1fadb2f489c0859996eb459499
totop716 <totop716@gmail.com>
2023-08-16 05:22:18 -0400

    fix transaction modals

--- src/pages/PoolsPage/v3/SupplyLiquidityV3/containers/AddLiquidityButton/index.tsx
+++ src/pages/PoolsPage/v3/SupplyLiquidityV3/containers/AddLiquidityButton/index.tsx
@@ -435,7 +435,9 @@ export function AddLiquidityButton({
                   console.error('Failed to send transaction', error);
                   setTxPending(false);
                   setAddLiquidityErrorMessage(
-                    error?.code === 4001 ? t('txRejected') : t('errorInTx'),
+                    error?.code === 'ACTION_REJECTED'
+                      ? t('txRejected')
+                      : t('errorInTx'),
                   );
                 }
               })
@@ -443,7 +445,9 @@ export function AddLiquidityButton({
                 console.error('Failed to send transaction', err);
                 setAttemptingTxn(false);
                 setAddLiquidityErrorMessage(
-                  err?.code === 4001 ? t('txRejected') : t('errorInTx'),
+                  err?.code === 'ACTION_REJECTED'
+                    ? t('txRejected')
+                    : t('errorInTx'),
                 );
               });
           })
@@ -453,9 +457,11 @@ export function AddLiquidityButton({
             setRejected && setRejected(true);
             setAttemptingTxn(false);
             setAddLiquidityErrorMessage(
-              error?.code === 4001 ? t('txRejected') : t('errorInTx'),
+              error?.code === 'ACTION_REJECTED'
+                ? t('txRejected')
+                : t('errorInTx'),
             );
-            if (error?.code !== 4001) {
+            if (error?.code !== 'ACTION_REJECTED') {
               console.error(error);
             }
           });
