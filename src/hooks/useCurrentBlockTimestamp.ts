c8a816c6a557379ea2463e5a36a45c0abd459212
totop716 <totop716@gmail.com>
2023-10-20 16:40:53 -0400

    fix hook to get block timestamp

--- src/hooks/useCurrentBlockTimestamp.ts
+++ src/hooks/useCurrentBlockTimestamp.ts
@@ -1,10 +1,10 @@
 import { BigNumber } from 'ethers';
-import { useSingleCallResult } from 'state/multicall/v3/hooks';
-import { useMulticall2Contract } from './useContract';
+import { useSingleCallResult } from 'state/multicall/hooks';
+import { useMulticallContract } from './useContract';
 
 // gets the current timestamp from the blockchain
 export default function useCurrentBlockTimestamp(): BigNumber | undefined {
-  const multicall = useMulticall2Contract();
+  const multicall = useMulticallContract();
   return useSingleCallResult(multicall, 'getCurrentBlockTimestamp')
     ?.result?.[0];
 }
