9fe5cffaf01a0cafbe02fffbf3e58911c9719e4b
stinso <marc.hilzendecker@gmail.com>
2024-02-01 03:03:06 +0100

    tf/feature/lpLock add v2 lock overview

--- src/pages/PoolsPage/lpLock/MyLiquidityLocks/index.tsx
+++ src/pages/PoolsPage/lpLock/MyLiquidityLocks/index.tsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Box, Button } from '@material-ui/core';
-import { useActiveWeb3React } from 'hooks';
+import { useActiveWeb3React, useV2LiquidityPools } from 'hooks';
 import { useTranslation } from 'react-i18next';
 import { useWalletModalToggle } from 'state/application/hooks';
 import ToggleVersion from '../ToggleVersion';
+import { useUserV2LiquidityLocks, useUserV3LiquidityLocks } from 'state/data/liquidityLocker';
+import { Skeleton } from '@material-ui/lab';
+import { LockPositionCard } from 'components';
 
 export default function MyLiquidityLocks() {
   const { t } = useTranslation();
   const [isV3, setIsV3] = useState(false);
-  const { account } = useActiveWeb3React();
+  // const { account } = useActiveWeb3React();
+  const account ="0x43affF626834561253C0fE4AC6B8B7dD44eEe68D"
+  const {
+    loading: v2IsLoading,
+    pairs: allV2PairsWithLiquidity,
+  } = useV2LiquidityPools(account ?? undefined);
+  const pairs = useMemo(() => allV2PairsWithLiquidity, [allV2PairsWithLiquidity.length]);
+  const { data: v2Locks, loading: v2LockIsLoading } = useUserV2LiquidityLocks(account, pairs)
 
   const showConnectAWallet = Boolean(!account);
-  const toggleWalletModal = useWalletModalToggle();
+  const toggleWalletModal = useWalletModalToggle();  
 
   return (
     <Box>
@@ -22,14 +32,36 @@ export default function MyLiquidityLocks() {
         <ToggleVersion method={setIsV3} checkValue={isV3} />
       </Box>
       <Box textAlign='center'>
-        <p>{t('noLiquidityLocks')}.</p>
-        {showConnectAWallet && (
+        {showConnectAWallet ? (
           <Box maxWidth={250} margin='20px auto 0'>
             <Button fullWidth onClick={toggleWalletModal}>
               {t('connectWallet')}
             </Button>
           </Box>
-        )}
+        ) : 
+          <Box mt={3}>
+            {(v2LockIsLoading || v2IsLoading) ? (
+              <Box width={1}>
+                <Skeleton width='100%' height={50} />
+              </Box>
+            ) : v2Locks && v2Locks.length > 0 ? (
+              <Box>
+                {v2Locks.map((lock) => (
+                  <Box key={lock.pair.tokenAddress} mt={2}>
+                    <LockPositionCard
+                      key={lock.pair.tokenAddress}
+                      lock={lock}
+                    />
+                  </Box>
+                ))}
+              </Box>
+            ) : (
+              <Box textAlign='center'>
+                <p>{t('noLiquidityLocks')}.</p>
+              </Box>
+            )}
+          </Box>
+        }
       </Box>
     </Box>
   );
--- src/state/data/liquidityLocker.ts
+++ src/state/data/liquidityLocker.ts
@@ -1,30 +1,161 @@
 import axios from 'axios';
-import { ChainId } from '@uniswap/sdk';
+import { ChainId, Pair as UniswapSdkPair } from '@uniswap/sdk';
 import { NONFUNGIBLE_POSITION_MANAGER_ADDRESSES } from 'constants/v3/addresses';
+import { useEffect, useState } from 'react';
 const TEAM_FINANCE_BACKEND_URL = 'https://team-finance-backend-dev-origdfl2wq-uc.a.run.app/api';
 const API_KEY = process.env.TEAM_FINANCE_BACKEND_API_KEY ?? 'yolobolo';
 const chain_id = ChainId.MATIC;
+
+export interface Event {
+  chainId: string
+  createdAt: string
+  isNftMinted: boolean
+  isWithdrawn: boolean
+  lockAmount: string
+  lockContractAddress: string
+  lockDepositId: number
+  lockNftContractAddress: string
+  migratedLockDepositId: number
+  network: string
+  senderAddress: string
+  timeStamp: number
+  tokenAddress: string
+  tokenId: string
+  tokenTotalSupply: string
+  transactionAmount: string
+  transactionHash: string
+  transactionIndex: number
+  unlockTime: number
+  updatedAt: string
+  withdrawalAddress: string
+}
+
+export interface Token {
+  chainId: string
+  createdAt: string
+  isLiquidityToken: boolean
+  isNFT: boolean
+  liquidityLockedInPercent: number
+  liquidityLockedInUsd: number
+  network: string
+  tokenAddress: string
+  tokenCirculatingSupply: string
+  tokenDecimals: number
+  tokenId: string
+  tokenLocked: string
+  tokenName: string
+  tokenImage: string
+  tokenSymbol: string
+  tokenTotalSupply: string
+  updatedAt: string
+}
+
+export interface LiquidityContract {
+  chainId: string
+  createdAt: string
+  isLiquidityToken: boolean
+  network: string
+  token0: string
+  token1: string
+  tokenAddress: string
+  tokenDecimals: number
+  tokenName: string
+  tokenSymbol: string
+  tokenTotalSupply: string
+  updatedAt: string
+}
+
+export interface Pair {
+  chainId: string
+  createdAt: string
+  isLiquidityToken: boolean
+  isNFT: boolean
+  liquidityLockedInPercent: number
+  liquidityLockedInUsd: number
+  network: string
+  tokenAddress: string
+  tokenCirculatingSupply: string
+  tokenDecimals: number
+  tokenId: string
+  tokenLocked: string
+  tokenName: string
+  tokenImage: string
+  tokenSymbol: string
+  tokenTotalSupply: string
+  updatedAt: string
+}
+
+export interface LockInterface {
+  event: Event
+  token: Token
+  liquidityContract: LiquidityContract
+  pair: Pair
+}
 
 axios.defaults.baseURL = TEAM_FINANCE_BACKEND_URL
 axios.defaults.timeout = 120000
 axios.defaults.headers.common.Accept = 'application/json'
 axios.defaults.headers.common.Authorization = API_KEY
 
-export const fetchUserV2LiquidityLocks = async (account: string, liquidityTokenList: any) => {
-    const addressArray = liquidityTokenList.map((item: any) => item.liquidityToken.address)
-    const URL = `/app/allMylocks/${account}`;
-    const response = await axios.get(URL);
-    return response.data.data.filter((item: any) => {
-        return (item.event.chainId == '0x89' && addressArray.includes(item.event.liquidityContractAddress))
-    });
+export const useUserV2LiquidityLocks = (account: string, liquidityTokenList: UniswapSdkPair[]) => {
+  const [data, setData] = useState<LockInterface[] | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const addressArray = liquidityTokenList.map((item: UniswapSdkPair) => item.liquidityToken.address)
+        const URL = `/app/allMylocks/${account}`;
+        const response = await axios.get(URL);
+        const v2LpLocks_ = response.data.data.filter((item: LockInterface) => {
+            return (item.event.chainId == '0x89' && addressArray.includes(item.liquidityContract.tokenAddress))
+        });
+        setData(v2LpLocks_);
+      } catch (error) {
+        setError("Error getting v2 lp locks");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (!account || !liquidityTokenList || liquidityTokenList.length === 0) return
+
+    fetchData();
+  }, [account, liquidityTokenList]);
+
+  return {data, loading, error}
 }
 
-export const fetchUserV3LiquidityLocks = async (account: string) => {
-    const URL = `/app/allMylocks/${account}`;
-    const response = await axios.get(URL);
-    return response.data.data.filter((item: any) => {
-        return (item.event.chainId == '0x89' && item.event.liquidityContractAddress == NONFUNGIBLE_POSITION_MANAGER_ADDRESSES[chain_id])
-    });
+export const useUserV3LiquidityLocks = (account?: string) => {
+  const [data, setData] = useState<LockInterface[] | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const URL = `/app/allMylocks/${account}`;
+        const response = await axios.get(URL);
+        const v3LpLocks_ = response.data.data.filter((item: any) => {
+          return (item.event.chainId == '0x89' && item.event.liquidityContractAddress == NONFUNGIBLE_POSITION_MANAGER_ADDRESSES[chain_id])
+        });
+        setData(v3LpLocks_);
+      } catch (error) {
+        setError("Error getting v3 lp locks");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (!account) return
+
+    fetchData();
+  }, [account]);
+
+  return {data, loading, error}
 }
 
 export const updateUserLiquidityLock = async (lockContractAddress: string, lockDepositId: number) => {
