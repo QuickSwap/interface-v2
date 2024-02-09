140a21089c27c691bb28a8ab2b0ec62f1a7e8cd0
totop716 <totop716@gmail.com>
2024-02-08 22:32:30 -0500

    show tooltip when hovering usd amount

--- src/pages/FarmPage/V3/MerklClaimAll.tsx
+++ src/pages/FarmPage/V3/MerklClaimAll.tsx
@@ -4,20 +4,26 @@ import ClaimAllBg from 'assets/images/claimAllBg.png';
 import { useTranslation } from 'react-i18next';
 import { useGetMerklFarms } from 'hooks/v3/useV3Farms';
 import { useUSDCPricesFromAddresses } from 'utils/useUSDCPrice';
-import { formatNumber } from 'utils';
+import { formatNumber, getTokenFromAddress } from 'utils';
 import { useClaimMerklRewards } from 'hooks/useClaimMerklRewards';
 import { Skeleton } from '@material-ui/lab';
+import { CurrencyLogo, CustomTooltip } from 'components';
+import { useSelectedTokenList } from 'state/lists/hooks';
+import { useActiveWeb3React } from 'hooks';
 
 export const MerklClaimAll: React.FC = () => {
   const { t } = useTranslation();
+  const { chainId } = useActiveWeb3React();
   const { isLoading: loadingFarms, data: merklFarms } = useGetMerklFarms();
+  const tokenMap = useSelectedTokenList();
 
   const rewards = useMemo(() => {
     if (!merklFarms) return [];
     return merklFarms.reduce((memo: any[], farm: any) => {
       const farmRewards = Object.keys(farm.rewardsPerToken).map((address) => {
         const rewardData = farm.rewardsPerToken[address];
-        return { ...rewardData, address };
+        const token = getTokenFromAddress(address, chainId, tokenMap, []);
+        return { ...rewardData, address, token };
       });
       farmRewards.forEach((reward) => {
         if (reward.unclaimed > 0) {
@@ -43,7 +49,7 @@ export const MerklClaimAll: React.FC = () => {
       });
       return memo;
     }, []);
-  }, [merklFarms]);
+  }, [chainId, merklFarms, tokenMap]);
 
   const {
     loading: loadingRewardTokenPrices,
@@ -68,7 +74,28 @@ export const MerklClaimAll: React.FC = () => {
         {loadingRewardTokenPrices || loadingFarms ? (
           <Skeleton variant='rect' width={100} height={27} />
         ) : (
-          <h6>${formatNumber(rewardsUSD)}</h6>
+          <CustomTooltip
+            placement='bottom-start'
+            title={
+              <Box className='flex flex-col' gridGap={8}>
+                {rewards.map((reward) => (
+                  <Box
+                    className='flex justify-between'
+                    width={200}
+                    key={reward.address}
+                  >
+                    <Box className='flex items-center' gridGap={8}>
+                      <CurrencyLogo size='16px' currency={reward.token} />
+                      <small>{reward.symbol}</small>
+                    </Box>
+                    <small>{formatNumber(reward.unclaimed)}</small>
+                  </Box>
+                ))}
+              </Box>
+            }
+          >
+            <h6>${formatNumber(rewardsUSD)}</h6>
+          </CustomTooltip>
         )}
       </Box>
       <Button disabled={claiming} onClick={claimReward}>
