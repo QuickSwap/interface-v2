a6a143db6935b7d22e5932ec9b45e68abe7fd536
webdev403 <web.dev8231@gmail.com>
2024-02-22 21:54:51 -0500

    update liquidity hub analytics page

--- src/components/LiquidityHubAnalytics/LiquidityHubAnalyticsTotal.tsx
+++ src/components/LiquidityHubAnalytics/LiquidityHubAnalyticsTotal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import { Skeleton } from '@material-ui/lab';
@@ -6,24 +6,59 @@ import Chart from 'react-apexcharts';
 import { formatCompact } from 'utils';
 import dayjs from 'dayjs';
 import { useLHAnalyticsDaily } from 'hooks/useLHAnalytics';
+import ChartType from 'components/ChartType';
+import { GlobalData, GlobalConst } from 'constants/index';
 
 const LiquidityHubAnalyticsTotal: React.FC = () => {
   const { t } = useTranslation();
+  const [durationIndex, setDurationIndex] = useState(
+    GlobalConst.analyticChart.ONE_MONTH_CHART,
+  );
 
   const { isLoading, data: lhData } = useLHAnalyticsDaily();
-  const data = lhData ?? [];
+  const data: any[] = lhData?.result?.rows ?? [];
+  const startTime =
+    durationIndex === GlobalConst.analyticChart.ALL_CHART
+      ? undefined
+      : dayjs(
+          dayjs()
+            .subtract(
+              durationIndex === GlobalConst.analyticChart.ONE_MONTH_CHART
+                ? 1
+                : durationIndex === GlobalConst.analyticChart.THREE_MONTH_CHART
+                ? 3
+                : durationIndex === GlobalConst.analyticChart.SIX_MONTH_CHART
+                ? 6
+                : 12,
+              'month',
+            )
+            .format('YYYY-MM-DD'),
+        ).unix();
+  const filteredData = data.filter((item) =>
+    startTime ? dayjs(item.evt_date).unix() >= startTime : true,
+  );
 
   return (
     <Box className='panel'>
-      <p>{t('liquidityHub')}</p>
+      <Box mb={2} className='flex flex-wrap justify-between' gridGap={8}>
+        <p>{t('liquidityHub')}</p>
+        <ChartType
+          typeTexts={GlobalData.analytics.CHART_DURATION_TEXTS}
+          chartTypes={GlobalData.analytics.CHART_DURATIONS}
+          chartType={durationIndex}
+          setChartType={setDurationIndex}
+        />
+      </Box>
       {isLoading ? (
         <Skeleton width='100%' height={400} />
-      ) : data && data.length > 0 ? (
+      ) : filteredData.length > 0 ? (
         <Chart
           series={[
             {
               name: 'Liquidity Hub',
-              data: data.map((item: any) => item.daily_total_calculated_value),
+              data: filteredData.map(
+                (item: any) => item.daily_total_calculated_value,
+              ),
             },
           ]}
           type='bar'
@@ -42,14 +77,14 @@ const LiquidityHubAnalyticsTotal: React.FC = () => {
               borderColor: 'rgba(130, 177, 255, 0.08)',
             },
             xaxis: {
-              categories: data.map((item: any) =>
+              categories: filteredData.map((item: any) =>
                 dayjs(item.evt_date).format('MMM DD'),
               ),
               labels: {
                 style: {
                   fontSize: '11px',
                   fontFamily: "'Inter', sans-serif",
-                  colors: data.map(() => '#696c80'),
+                  colors: filteredData.map(() => '#696c80'),
                 },
               },
               axisTicks: {
