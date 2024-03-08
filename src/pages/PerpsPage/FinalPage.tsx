c16845050901c951b32f4a255c8dc7f729899a2b
smrnjeet222 <smrnjeet.dev@gmail.com>
2024-03-08 10:43:10 +0530

    Laptop and Tablet UI fixed

--- src/pages/PerpsPage/FinalPage.tsx
+++ src/pages/PerpsPage/FinalPage.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Flex, Container, Text } from '@radix-ui/themes';
 import { AdvancedChart } from 'react-tradingview-embed';
 import { Market } from './Market';
 import { Leverage } from './Leverage';
 import { GraphHeader } from './GraphHeader';
 import { Orderbook } from './Orderbook';
-
+import './FinalPage.css';
 export const FinalPage: React.FC = () => {
   const [currentTab, setCurrentTab] = useState('market');
   const handleTabChange = (tab: string) => {
@@ -13,96 +13,199 @@ export const FinalPage: React.FC = () => {
   };
   const [token, setToken] = useState('PERP_ETH_USDC');
   const [selectedOption, setSelectedOption] = useState('Portfolio');
-
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const handleOptionClick = (option) => {
     setSelectedOption(option);
   };
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <>
       <Flex
-        direction='row'
-        justify='between'
+        direction={windowWidth <= 1024 ? 'column' : 'row'}
+        justify='center'
         align='start'
         style={{
           width: '100vw',
-          height: '100vh',
+          height: '200vh',
         }}
       >
         <Container
+          id='graph'
           style={{
-            width: '61vw',
+            width: windowWidth <= 1024 ? '100vw' : '61vw',
+            height: windowWidth <= 1024 ? 'fit-content' : '100vh',
           }}
         >
           <GraphHeader setTokenName={setToken} />
-          <AdvancedChart />
-        </Container>
-        <Container
-          style={{
-            width: '15.5vw',
-            height: '86vh',
-            border: '1px solid  #1b1e29',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              textAlign: 'center',
-              width: '100%',
-              marginBottom: '5px',
+          <AdvancedChart
+            widgetProps={{
+              theme: 'dark',
+              style: '9',
             }}
-          >
-            <div
-              onClick={() => handleTabChange('market')}
-              style={{
-                flex: 1,
-                cursor: 'pointer',
-                padding: '5px 0',
-                fontFamily: 'Inter',
-                fontWeight: 500,
-                borderBottom:
-                  currentTab === 'market' ? '2px solid white' : 'none',
-                color: currentTab === 'market' ? 'white' : '#61657a',
-              }}
-            >
-              Market
-            </div>
-            <div
-              onClick={() => handleTabChange('orderbook')}
-              style={{
-                flex: 1,
-                cursor: 'pointer',
-                padding: '5px 0',
-                fontFamily: 'Inter',
-                fontWeight: 500,
-                borderBottom:
-                  currentTab === 'orderbook' ? '2px solid white' : 'none',
-                color: currentTab === 'orderbook' ? 'white' : '#61657a',
-              }}
-            >
-              Orderbook
-            </div>
-          </div>
-          {currentTab === 'market' && <Market token={token} />}
-          {currentTab === 'orderbook' && <Orderbook token={token} />}
+          />
         </Container>
 
-        <Container
-          style={{
-            width: '21vw',
-            height: '100vh',
-          }}
-        >
-          <Leverage />
-        </Container>
+        {windowWidth <= 1024 ? (
+          <div
+            id='test'
+            style={{
+              display: 'flex',
+              width: '100vw',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <Container
+              id='market'
+              style={{
+                width: '50w',
+                flex: 1,
+                height: '100vh',
+                border: '1px solid  #1b1e29',
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  textAlign: 'center',
+                  width: '100%',
+                  marginBottom: '5px',
+                }}
+              >
+                <div
+                  onClick={() => handleTabChange('market')}
+                  style={{
+                    flex: 1,
+                    cursor: 'pointer',
+                    padding: '5px 0',
+                    fontFamily: 'Inter',
+                    fontWeight: 500,
+                    borderBottom:
+                      currentTab === 'market' ? '2px solid white' : 'none',
+                    color: currentTab === 'market' ? 'white' : '#61657a',
+                  }}
+                >
+                  Market
+                </div>
+                <div
+                  onClick={() => handleTabChange('orderbook')}
+                  style={{
+                    flex: 1,
+                    cursor: 'pointer',
+                    padding: '5px 0',
+                    fontFamily: 'Inter',
+                    fontWeight: 500,
+                    borderBottom:
+                      currentTab === 'orderbook' ? '2px solid white' : 'none',
+                    color: currentTab === 'orderbook' ? 'white' : '#61657a',
+                  }}
+                >
+                  Orderbook
+                </div>
+              </div>
+              {currentTab === 'market' && <Market token={token} />}
+              {currentTab === 'orderbook' && <Orderbook token={token} />}
+            </Container>
+            <Container
+              id='leverage'
+              style={{
+                width: '50vw',
+                flex: 1,
+              }}
+            >
+              <div style={{ marginTop: '-92px' }}>
+                <Leverage />
+              </div>
+            </Container>
+          </div>
+        ) : (
+          <>
+            <Container
+              id='market'
+              style={{
+                width: '15.5vw',
+                height: '86vh',
+                border: '1px solid  #1b1e29',
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  textAlign: 'center',
+                  width: '100%',
+                  marginBottom: '5px',
+                }}
+              >
+                <div
+                  onClick={() => handleTabChange('market')}
+                  style={{
+                    flex: 1,
+                    cursor: 'pointer',
+                    padding: '5px 0',
+                    fontFamily: 'Inter',
+                    fontWeight: 500,
+                    borderBottom:
+                      currentTab === 'market' ? '2px solid white' : 'none',
+                    color: currentTab === 'market' ? 'white' : '#61657a',
+                  }}
+                >
+                  Market
+                </div>
+                <div
+                  onClick={() => handleTabChange('orderbook')}
+                  style={{
+                    flex: 1,
+                    cursor: 'pointer',
+                    padding: '5px 0',
+                    fontFamily: 'Inter',
+                    fontWeight: 500,
+                    borderBottom:
+                      currentTab === 'orderbook' ? '2px solid white' : 'none',
+                    color: currentTab === 'orderbook' ? 'white' : '#61657a',
+                  }}
+                >
+                  Orderbook
+                </div>
+              </div>
+              {currentTab === 'market' && <Market token={token} />}
+              {currentTab === 'orderbook' && <Orderbook token={token} />}
+            </Container>
+
+            <Container
+              id='leverage'
+              style={{
+                width: '21vw',
+                height: '100vh',
+              }}
+            >
+              <Leverage />
+            </Container>
+          </>
+        )}
       </Flex>
-      <Flex direction='column' style={{ width: '77.5vw', height: '50vh' }}>
+      <Flex
+        direction='column'
+        style={{
+          width: windowWidth <= 1024 ? '100vw' : '77.5vw',
+          height: '50vh',
+          marginTop: windowWidth <= 1024 ? '0px' : '-715px',
+        }}
+      >
         <Flex
           direction={'row'}
           justify={'between'}
           align={'center'}
           style={{
-            width: '77.5vw',
             padding: '10px',
             border: '1px solid  #1b1e29',
           }}
@@ -111,8 +214,9 @@ export const FinalPage: React.FC = () => {
             direction='row'
             justify='between'
             align='center'
+            gap={'3'}
             style={{
-              width: '40vw',
+                width: windowWidth <= 1024 ? '60vw' : '40vw',
               fontSize: '14px',
               fontFamily: 'Inter',
               fontWeight: 500,
@@ -159,7 +263,7 @@ export const FinalPage: React.FC = () => {
           align='center'
           style={{
             border: '1px solid  #1b1e29',
-            width: '77.5vw',
+            width: windowWidth <= 1024 ? '100vw' : '77.5vw',
             height: '80px',
             padding: '10px',
           }}
@@ -236,7 +340,7 @@ export const FinalPage: React.FC = () => {
           justify='between'
           align='start'
           style={{
-            width: '77.5vw',
+            width: windowWidth <= 1024 ? '100vw' : '77.5vw',
             height: '100px',
             padding: '5px 10px 0 10px',
             border: '1px solid #1b1e29',
@@ -262,7 +366,7 @@ export const FinalPage: React.FC = () => {
           justify='center'
           align='center'
           style={{
-            width: '77.5vw',
+            width: windowWidth <= 1024 ? '100vw' : '77.5vw',
             height: '150px',
             border: '1px solid #1b1e29',
           }}
