e3583bee6e21d1f993ed0e0734c614151187e2b3
jeet <smrnjeet.dev@gmail.com>
2024-03-11 14:31:53 +0530

    css fix

--- src/pages/PerpsPage/OrderbookV2.tsx
+++ src/pages/PerpsPage/OrderbookV2.tsx
@@ -1,18 +1,16 @@
 import { useState } from 'react';
 import { OrderBook } from '@orderly.network/react';
 import { useOrderbookStream, useSymbolsInfo } from '@orderly.network/hooks';
-import '@orderly.network/react/dist/styles.css';
+
 export const OrderbookV2 = () => {
   const [symbol, setSymbol] = useState('PERP_ETH_USDC');
   const config = useSymbolsInfo();
   const symbolInfo = config ? config[symbol] : {};
 
-  const [
-    data,
-    { onDepthChange, isLoading, onItemClick, depth, allDepths },
-  ] = useOrderbookStream(symbol, undefined, {
-    level: 7,
-  });
+  const [data, { onDepthChange, isLoading, onItemClick, depth, allDepths }] =
+    useOrderbookStream(symbol, undefined, {
+      level: 7,
+    });
 
   return (
     <div style={{ width: 300, height: 480 }}>
