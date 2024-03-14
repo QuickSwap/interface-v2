9037781702caf2d2cbbfca30d30a5eeb4535fdf3
totop716 <totop716@gmail.com>
2024-03-14 11:13:48 -0400

    Fix merkl farm card and routing tokens for zkevm

--- src/hooks/v3/useV3Farms.ts
+++ src/hooks/v3/useV3Farms.ts
@@ -314,9 +314,8 @@ export const useGetMerklFarms = () => {
     const merklAPIURL = process.env.REACT_APP_MERKL_API_URL;
     if (!merklAPIURL || !chainId || !merklAvailable) return [];
     const amms = merklAMMs[chainId] ?? ['quickswapuni'];
-    const ammStr = amms.map((amm) => `&AMMs[]=${amm}`).join('');
     const res = await fetch(
-      `${merklAPIURL}?chainIds[]=${chainId}${ammStr}${
+      `${merklAPIURL}?chainIds[]=${chainId}${
         account ? `&user=${account}` : ''
       }`,
     );
@@ -451,7 +450,7 @@ export const useMerklFarms = () => {
                 0,
               ),
             almAPR: item?.meanAPR ?? 0,
-            label: 'QuickSwap',
+            label: item?.ammName,
           },
         ])
         .map((alm: any) => {
@@ -502,7 +501,10 @@ export const useMerklFarms = () => {
                   e.strategy.address.toLowerCase() ===
                   alm.almAddress.toLowerCase(),
               )?.strategy?.fees_apr ?? 0;
-          } else if (alm.label.includes('QuickSwap') && eternalFarmPoolAprs) {
+          } else if (
+            alm.label.toLowerCase().includes('quickswap') &&
+            eternalFarmPoolAprs
+          ) {
             poolAPR = eternalFarmPoolAprs[alm.almAddress.toLowerCase()] ?? 0;
           }
           return {
