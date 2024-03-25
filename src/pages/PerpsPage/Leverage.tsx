7ea860007f66ee7940f4c2816f794b81f52afdc1
totop716 <totop716@gmail.com>
2024-03-25 14:07:05 -0400

    Fix leverage component

--- src/pages/PerpsPage/Leverage.tsx
+++ src/pages/PerpsPage/Leverage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import Arrow from '../../assets/images/downward.svg';
+import Arrow from 'assets/images/downward.svg';
 import { CropSquareOutlined } from '@material-ui/icons';
-import { useActiveWeb3React } from '../../hooks';
+import { useActiveWeb3React } from 'hooks';
 import {
   useAccount,
   useChains,
@@ -10,13 +10,7 @@ import {
   useOrderEntry,
 } from '@orderly.network/hooks';
 import AssetModal from '../../components/AssetModal';
-import {
-  AccountStatusEnum,
-  OrderSide,
-  OrderStatus,
-  OrderType,
-  OrderEntity,
-} from '@orderly.network/types';
+import { AccountStatusEnum } from '@orderly.network/types';
 import AccountModal from '../../components/AccountModal';
 import { Simulate } from 'react-dom/test-utils';
 import submit = Simulate.submit;
@@ -40,16 +34,7 @@ export const Leverage: React.FC<{ perpToken: string; orderQuantity: any }> = ({
     srcToken: token?.symbol,
     srcChainId: Number(chainId),
   });
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+
   const [modalOpen, setModalOpen] = useState(false);
   const [accountModalOpen, setAccountModalOpen] = useState(false);
   const collateral = useCollateral();
@@ -86,88 +71,63 @@ export const Leverage: React.FC<{ perpToken: string; orderQuantity: any }> = ({
     }
   }, [state.status]);
   return (
-    <Box>
-      <Box
-        style={{
-          width: 'fit-content',
-          height: 'max-content',
-          backgroundColor: '#12131a',
-          border: '1px solid  #1b1e29',
-        }}
-      >
-        {windowWidth > 768 && (
-          <Box className='flex items-center justify-between' gridGap={8}>
-            <Box>
-              <p className='span text-secondary'>Total Balance</p>
-              <p className='span'>
-                {deposit?.balance}{' '}
-                <span className='text-secondary'>{token?.symbol}</span>
-              </p>
-            </Box>
-            <Box className='flex items-center'>
-              <Button
-                disabled={!quickSwapAccount}
-                onClick={() => {
-                  setModalOpen(true);
-                }}
-              >
-                Manage
-              </Button>
-              <img src={Arrow} width='16' height='16' />
-            </Box>
+    <>
+      <Box padding='15px 10px'>
+        <Box className='flex items-center justify-between' gridGap={8}>
+          <Box>
+            <p className='span text-secondary'>Total Balance</p>
+            <p className='span'>
+              {deposit?.balance}{' '}
+              <span className='text-secondary'>{token?.symbol}</span>
+            </p>
           </Box>
-        )}
-        {windowWidth > 768 && (
-          <div>
-            <Box
-              style={{
-                width: 220,
-                height: 0.5,
-                backgroundColor: '#696C80',
-                margin: '10px 15px',
+          <Box className='flex items-center' gridGap={8}>
+            <Button
+              className='leverageManageButton'
+              disabled={!quickSwapAccount}
+              onClick={() => {
+                setModalOpen(true);
               }}
-            />
-
-            <Box
-              style={{
-                width: 220,
-                height: 8,
-                borderRadius: '8px',
-                backgroundImage:
-                  'linear-gradient(to right, #24403a, #3f3c2f 48%, #402230)',
-                margin: '0px 15px',
-              }}
-            />
-            <Box className='flex justify-between' gridGap={8}>
-              <Box>
-                <p className='span text-secondary'>Account Leverage</p>
-                <p className='span'>0.00x/-x</p>
-              </Box>
-              <Box textAlign='right'>
-                <p className='span text-secondary'>Margin Ratio</p>
-                <p className='span'>100%</p>
-              </Box>
-            </Box>
-            <Box
-              style={{
-                width: 220,
-                height: 0.5,
-                backgroundColor: '#696C80',
-                margin: '10px 15px',
-              }}
-            />
-          </div>
-        )}
-        <Box className='flex'>
-          <Box onClick={() => setOrder({ ...order, side: 'BUY' })}>
-            Buy/Long
-          </Box>
-          <Box onClick={() => setOrder({ ...order, side: 'SELL' })}>
-            Sell/Short
+            >
+              Manage
+            </Button>
+            <img src={Arrow} width='16' height='16' />
           </Box>
         </Box>
-        <Box className='flex justify-between'>
-          <p className='span'>Available {collateral.availableBalance} USDC</p>
+        <Box
+          padding='15px 0'
+          margin='15px 0'
+          className='border-top border-bottom flex flex-col'
+          gridGap={12}
+        >
+          <Box className='leverageGradient' />
+          <Box className='flex justify-between' gridGap={8}>
+            <Box>
+              <p className='span text-secondary'>Account Leverage</p>
+              <p className='span'>0.00x/-x</p>
+            </Box>
+            <Box textAlign='right'>
+              <p className='span text-secondary'>Margin Ratio</p>
+              <p className='span'>100%</p>
+            </Box>
+          </Box>
+        </Box>
+        <Box className='leverageTypesWrapper' gridGap={2}>
+          <Box onClick={() => setOrder({ ...order, side: 'BUY' })}>
+            <span className='text-secondary'>Buy/Long</span>
+          </Box>
+          <Box onClick={() => setOrder({ ...order, side: 'SELL' })}>
+            <span className='text-secondary'>Sell/Short</span>
+          </Box>
+        </Box>
+        <Box className='flex justify-between' mt={2}>
+          <p className='span text-secondary'>
+            Available{' '}
+            <span className='text-primaryText'>
+              {collateral.availableBalance}
+            </span>{' '}
+            USDC
+          </p>
           <p
             className='span text-primary cursor-pointer'
             onClick={() => quickSwapAccount && setModalOpen(true)}
@@ -175,7 +135,7 @@ export const Leverage: React.FC<{ perpToken: string; orderQuantity: any }> = ({
             Deposit
           </p>
         </Box>
-        <Box className='flex' gridGap={16}>
+        <Box className='flex' gridGap={16} mt={2}>
           <p
             className={`span cursor-pointer ${
               order.order_type === 'LIMIT' ? '' : 'text-secondary'
@@ -193,24 +153,10 @@ export const Leverage: React.FC<{ perpToken: string; orderQuantity: any }> = ({
             Market
           </p>
         </Box>
-        <Box
-          style={{
-            width: 220,
-            height: 36,
-            backgroundColor: '#1b1e29',
-            margin: '16px 15px',
-            fontWeight: 500,
-            fontFamily: 'Inter',
-            cursor: 'pointer',
-            fontSize: '12px',
-            borderRadius: '8px',
-            color: '#61657a',
-            padding: '10px 12px 11px',
-          }}
-        >
-          <p>Price</p>
-          <div>
-            {order.order_type === 'LIMIT' ? (
+        {order.order_type === 'LIMIT' && (
+          <Box className='leverageInputWrapper' mt={2}>
+            <span className='text-secondary'>Price</span>
+            <Box gridGap={5}>
               <input
                 min={0}
                 disabled={state.status !== AccountStatusEnum.EnableTrading}
@@ -218,39 +164,14 @@ export const Leverage: React.FC<{ perpToken: string; orderQuantity: any }> = ({
                 onChange={(e) =>
                   setOrder({ ...order, order_price: e.target.value })
                 }
-                style={{
-                  width: 30,
-                  marginRight: '2px',
-                  color: '#696c80',
-                  border: 'none',
-                  outline: 'none',
-                  appearance: 'none',
-                  backgroundColor: 'transparent',
-                }}
               />
-            ) : (
-              <p>Market</p>
-            )}
-            <p> USDC</p>
-          </div>
-        </Box>
-        <Box
-          style={{
-            width: 220,
-            height: 36,
-            backgroundColor: '#1b1e29',
-            margin: '16px 15px',
-            fontWeight: 500,
-            fontFamily: 'Inter',
-            cursor: 'pointer',
-            fontSize: '12px',
-            borderRadius: '8px',
-            color: '#61657a',
-            padding: '10px 12px 11px',
-          }}
-        >
-          <p>Quantity</p>
-          <div>
+              <span className='text-secondary'>USDC</span>
+            </Box>
+          </Box>
+        )}
+        <Box className='leverageInputWrapper' mt={2}>
+          <span className='text-secondary'>Quantity</span>
+          <Box gridGap={5}>
             <input
               min={0}
               value={order.order_quantity}
@@ -258,150 +179,56 @@ export const Leverage: React.FC<{ perpToken: string; orderQuantity: any }> = ({
                 setOrder({ ...order, order_quantity: e.target.value })
               }
               disabled={state.status !== AccountStatusEnum.EnableTrading}
-              style={{
-                width: 30,
-                marginRight: '2px',
-                color: '#696c80',
-                border: 'none',
-                outline: 'none',
-                appearance: 'none',
-                backgroundColor: 'transparent',
-              }}
             />
-            <p>{perpToken.split('_')[1] || 'ETH'}</p>
-          </div>
+            <span className='text-secondary'>
+              {perpToken.split('_')[1] || 'ETH'}
+            </span>
+          </Box>
         </Box>
-        <Box style={{ margin: '10px 15px' }}>
-          <p
-            style={{
-              fontSize: '12px',
-              fontFamily: 'Inter',
-              fontWeight: '500',
-              color: '#61657a',
-            }}
-          >
-            0%
+        <Box className='leverageSquareWrapper' my={2}>
+          <CropSquareOutlined
+            fontSize='small'
+            style={{ transform: 'rotate(45deg)' }}
+          />
+          <Box />
+          <CropSquareOutlined
+            fontSize='small'
+            style={{ transform: 'rotate(45deg)' }}
+          />
+          <Box />
+          <CropSquareOutlined
+            fontSize='small'
+            style={{ transform: 'rotate(45deg)' }}
+          />
+          <Box />
+          <CropSquareOutlined
+            fontSize='small'
+            style={{ transform: 'rotate(45deg)' }}
+          />
+          <Box />
+          <CropSquareOutlined
+            fontSize='small'
+            style={{ transform: 'rotate(45deg)' }}
+          />
+        </Box>
+        <Box className='flex justify-between'>
+          <p className='span text-success'>0%</p>
+          <p className='span text-secondary'>
+            Max buy <span className='text-success'>0.000</span>
           </p>
-          <p
-            style={{
-              fontSize: '12px',
-              fontFamily: 'Inter',
-              fontWeight: '500',
-              color: '#61657a',
-            }}
-          >
-            Max buy 0.000
-          </p>
         </Box>
-        <Box
-          style={{
-            margin: '15px 15px',
-            cursor: 'pointer',
-          }}
-        >
-          <CropSquareOutlined
-            fontSize='small'
-            style={{ transform: 'rotate(45deg)' }}
-          />
-          <Box
-            style={{
-              width: 30,
-              height: 0.5,
-              backgroundColor: '#696C80',
-            }}
-          />
-          <CropSquareOutlined
-            fontSize='small'
-            style={{ transform: 'rotate(45deg)' }}
-          />
-          <Box
-            style={{
-              width: 30,
-              height: 0.5,
-              backgroundColor: '#696C80',
-            }}
-          />
-          <CropSquareOutlined
-            fontSize='small'
-            style={{ transform: 'rotate(45deg)' }}
-          />
-          <Box
-            style={{
-              width: 30,
-              height: 0.5,
-              backgroundColor: '#696C80',
-            }}
-          />
-          <CropSquareOutlined
-            fontSize='small'
-            style={{ transform: 'rotate(45deg)' }}
-          />
-          <Box
-            style={{
-              width: 30,
-              height: 0.5,
-              backgroundColor: '#696C80',
-            }}
-          />
-          <CropSquareOutlined
-            fontSize='small'
-            style={{ transform: 'rotate(45deg)' }}
-          />
+        <Box className='leverageInputWrapper flex justify-between' my={2}>
+          <span className='text-secondary'>Total</span>
+          <span className='text-secondary'>USDC</span>
         </Box>
-        <Box
-          style={{
-            width: 220,
-            height: 36,
-            backgroundColor: '#1b1e29',
-            margin: '16px 15px',
-            fontWeight: 500,
-            fontFamily: 'Inter',
-            fontSize: '12px',
-            borderRadius: '8px',
-            cursor: 'pointer',
-            padding: '10px 12px 11px',
-            color: '#61657a',
-          }}
-        >
-          <p>Total</p>
-          <p>USDC</p>
-        </Box>
-        <Box
-          style={{
-            width: 220,
-            height: 0.5,
-            backgroundColor: '#696C80',
-            margin: '10px 15px',
-          }}
-        />
-        <Box
-          style={{
-            margin: '10px 15px',
-            fontWeight: 500,
-            fontFamily: 'Inter',
-            fontSize: '12px',
-            borderRadius: '8px',
-            color: '#61657a',
-          }}
-        >
-          <p>
-            <Box>
-              <Switch defaultChecked /> Reduce Only
-            </Box>
-          </p>
+        <Box className='border-top flex justify-between items-center'>
+          <Box>
+            <Switch defaultChecked /> Reduce Only
+          </Box>
           <img src={Arrow} width='16' height='16' />
         </Box>
         {state.status !== AccountStatusEnum.EnableTrading ? (
           <Button
-            style={{
-              width: 220,
-              height: 40,
-              margin: '16px 15px',
-              padding: '11px 50px 12px',
-              borderRadius: '8px',
-              backgroundColor: 'rgb(4, 109, 4)',
-              cursor: 'pointer',
-            }}
             onClick={() => {
               setAccountModalOpen(true);
             }}
@@ -438,6 +265,6 @@ export const Leverage: React.FC<{ perpToken: string; orderQuantity: any }> = ({
         open={accountModalOpen}
         onClose={() => setModalOpen(false)}
       />
-    </Box>
+    </>
   );
 };
