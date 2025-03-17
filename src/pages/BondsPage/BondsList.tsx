e9237c859ad882cc8f6a06152ee7c42744971c48
brianshattuck <ubi.eng827@gmail.com>
2025-03-17 11:28:36 -0700

    remove a console log

--- src/pages/BondsPage/BondsList.tsx
+++ src/pages/BondsPage/BondsList.tsx
@@ -106,7 +106,6 @@ const BondsList: React.FC<BondsListProps> = ({ search }) => {
 
   const filteredUserBonds = useMemo(() => {
     if (!userBonds) return [];
-    console.log('11111111', userBonds);
     return userBonds
       .filter((userBond) => {
         const { bond } = userBond;
